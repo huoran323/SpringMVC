bf4f9e1dea0a93f194f9eb167ec11e0b0844d4b5
xiaoran <303176152@qq.com>
2020-03-19 14:33:16 +0800

    文件下载

---
+++ src/com/hr/controller/TestUploadAndDownController.java
@@ -0,0 +1,44 @@
+package com.hr.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestUploadAndDownController {
+
+	@RequestMapping("/down")
+	public ResponseEntity<byte[]> down(HttpSession session) throws IOException{
+		
+		//获取下载文件的路径:获取项目中img文件的路径
+		String realPath = session.getServletContext().getRealPath("img");
+		String finalPath = realPath + File.separator + "1.jpg";
+		InputStream is = new FileInputStream(finalPath);
+		
+		//available()：获取输入流所读取的文件的最大字节数，即获取1.jpg文件的字节数
+		byte[] b = new byte[is.available()];
+		is.read(b);
+		
+		//设置请求头
+		HttpHeaders headers = new HttpHeaders();
+		//attachment:以附件的形式下载；filename：为所下载的文件设置默认的名字
+		headers.add("Content-Disposition", "attachment;filename=abc.jpg");
+		
+		//设置响应状态
+		HttpStatus statusCode = HttpStatus.OK;
+		
+		ResponseEntity<byte[]> entity = new ResponseEntity<byte[]>(b, headers, statusCode);
+		
+		is.close();
+		return entity;
+	}
+}
